14cf409aaa14bbd1966219670f6b9e1bcc9e4116
psteeep <psteeep@users.noreply.github.com>
2022-06-27 14:02:29 +0300

    remake model relations

--- board/models.py
+++ board/models.py
@@ -7,11 +7,14 @@ class Direction(models.Model):
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     sort_num = models.IntegerField()
 
+    def __str__(self):
+        return self.direction_name
+
 
 class Doctor(models.Model):
     doctor_name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
-    direction = models.ForeignKey("Direction", on_delete=models.CASCADE)
+    direction = models.ManyToManyField(Direction)
     description = models.CharField(max_length=100)
     birth_date = models.DateTimeField("data of birth")
     work_exp = models.IntegerField()
